7592d7728c9e0d9ebeb3cf21811d3d52bffd3ccb
Paulo <zhonions1234@gmail.com>
2023-12-11 19:51:13 +0000

    background change v1

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,3 +1,5 @@
+import FrontEnd.Colors;
+
 import java.util.Scanner;
 
 public class GameManager {
@@ -27,7 +29,7 @@ public class GameManager {
                 default:
                     System.out.println("Enter a valid option:");
             }
-        } while(!choice.equals("0"));
+        } while (!choice.equals("0"));
     }
 
     public void playMenu() {
@@ -37,6 +39,7 @@ public class GameManager {
             System.out.println("PLAY Menu");
             System.out.println();
             System.out.println("1. PLAY");
+            System.out.println("2. Change your player background");
             System.out.println("0. Exit the program");
             System.out.println();
             System.out.print("Enter choice: ");
@@ -45,13 +48,55 @@ public class GameManager {
                 case "1":
                     play();
                     break;
+                case "2":
+                    choiceBackground();
+                    break;
                 case "0":
                     System.out.println("Exit...");
                     break;
                 default:
                     System.out.println("Enter a valid option:");
             }
-        } while(!choice.equals("0"));
+        } while (!choice.equals("0"));
+    }
+
+    public void choiceBackground() {
+        Scanner scan = new Scanner(System.in);
+        User user = new User();
+        String choice;
+        System.out.println("Background player Menu");
+        System.out.println();
+        System.out.println("Default Background: " + Colors.BLUE_BACKGROUND_BRIGHT + "◻️" + Colors.RESET);
+        System.out.println();
+        System.out.println("1. " + Colors.YELLOW_BACKGROUND + "◻️" + Colors.RESET);
+        System.out.println("2. " + Colors.CYAN_BACKGROUND + "◻️" + Colors.RESET);
+        System.out.println("3. " + Colors.RED_BACKGROUND + "◻️" + Colors.RESET);
+        System.out.println("4. " + Colors.GREEN_BACKGROUND + "◻️" + Colors.RESET);
+        System.out.println("0. Exit the program");
+        System.out.println();
+        System.out.print("Enter choice: ");
+        choice = scan.next();
+
+        switch (choice) {
+            case "1":
+                user.setPlayerBackgroundColor(Colors.YELLOW_BACKGROUND + "◻️" + Colors.RESET);
+                break;
+            case "2":
+                user.setPlayerBackgroundColor(Colors.CYAN_BACKGROUND + "◻️" + Colors.RESET);
+                break;
+            case "3":
+                user.setPlayerBackgroundColor(Colors.RED_BACKGROUND+ "◻️" + Colors.RESET);
+                break;
+            case "4":
+                user.setPlayerBackgroundColor(Colors.GREEN_BACKGROUND + "◻️" + Colors.RESET);
+                break;
+            case "0":
+                System.out.println("Exit...");
+                break;
+            default:
+                System.out.println("Enter a valid option:");
+        }
+        playMenu();
     }
 
     public static void play() {
@@ -71,8 +116,8 @@ public class GameManager {
             System.out.println();
             System.out.print("Enter choice: ");
             playerMove = scan.next();
-            map.checkUserMove(user,playerMove.toUpperCase());
-        } while(!playerMove.equals("0"));
+            map.checkUserMove(user, playerMove.toUpperCase());
+        } while (!playerMove.equals("0"));
     }
 
 }
