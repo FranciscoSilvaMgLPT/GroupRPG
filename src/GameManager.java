e61789891a7ae5ac43845d3ee41095fcd0b7c4da
Ricardo Silva <ricardojrs.mindera@gmail.com>
2023-12-11 09:57:12 +0000

    updated gamemanager

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,11 +1,37 @@
+import java.util.Scanner;
+
 public class GameManager {
-    public void gameManager(){
+    public void gameManager() {
+        Scanner scan = new Scanner(System.in);
         while (true) {
-            Menus menus = new Menus();
-            menus.menuRegisterLogin();
+            RegisterLogin registerLogin = new RegisterLogin();
+            String choice;
+            System.out.println("Main Menu");
+            System.out.println();
+            System.out.println("1. Register");
+            System.out.println("2. Login");
+            System.out.println("0. Exit the program");
+            System.out.println();
+            System.out.print("Enter choice: ");
+            choice = scan.next();
+            switch (choice) {
+                case "1":
+                    registerLogin.register();
+                    break;
+                case "2":
+                    registerLogin.login();
+                    break;
+                case "0":
+                    System.out.println("Exit...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Enter a valid option:");
+            }
         }
     }
-    public static void showMap(){
+
+    public static void showMap() {
         Map map = new Map();
         map.createMap();
         map.CreateLimits();
