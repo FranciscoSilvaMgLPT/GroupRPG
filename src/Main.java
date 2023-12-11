d7656d52629a9ffa1d54ba0212494974e1fd1056
Ricardo Silva <ricardojrs.mindera@gmail.com>
2023-12-11 10:17:36 +0000

    updated gamemanager

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-    GameManager gameManager = new GameManager();
-    gameManager.gameManager();
-
+        GameManager gameManager = new GameManager();
+        gameManager.initialMenu();
 
     }
 }
